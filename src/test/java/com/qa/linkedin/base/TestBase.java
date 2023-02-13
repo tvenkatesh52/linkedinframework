cd5b0f47f97958688dae181f6b958c4c36c00472
tvenkatesh52 <112702584+tvenkatesh52@users.noreply.github.com>
2023-02-13 14:24:28 +0530

    update changes

--- src/test/java/com/qa/linkedin/base/TestBase.java
+++ src/test/java/com/qa/linkedin/base/TestBase.java
@@ -22,7 +22,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class TestBase {
 	//read data from local file for practice git command modified
-	//1.changes happening in remote repository to local  repository
+	
     //2.changes are happening local repository to remote repository
 	public static WebDriver driver=null;
 	public static WebDriverWait wait=null;
